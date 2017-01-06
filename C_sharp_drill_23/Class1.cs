18f05b72ac23841238df83453b28be4f958c8536
Tranel Bland <tranelb@gmail.com>
2017-01-06 12:05:47 -0800

    23 drill rough

---
+++ C_sharp_drill_23/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_drill_23
+{
+    class Class1
+    {
+        static void Main()
+        {
+
+            /*
+            //Overloading is when you have multiple methods in the same scope, with the same name but different signatures.
+            public class test
+            {
+                public void getStuff(int id)
+                { }
+                public void getStuff(string name)
+                { }
+            }
+
+
+            //end
+
+            //Overriding
+            public class test
+            {
+                public virtual void getStuff(int id)
+                {
+                    //Get stuff default location
+                }
+            }
+
+            public class test2 : test
+            {
+                public override void getStuff(int id)
+                {
+                    //base.getStuff(id);
+                    //or - Get stuff new location
+                }
+            }
+
+
+            //poooop
+            */
+
+        }
+
+    }
+}
